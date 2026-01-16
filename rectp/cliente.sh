e87635361f1404a36591ec21869e37b75d5e00cc
ENTI <martin.pastor@estudiant.enti.cat>
2026-01-16 13:10:57 +0100

    commit

--- rectp/cliente.sh
+++ rectp/cliente.sh
@@ -1,64 +1,86 @@
 #!/bin/bash
-PORT=9999
-VERSION_CURRENT="0.6"
+
 AUDIO_FILE="audio.wav"
-IP_SERVER="192.168.225.96"
+HASH_MD5_HEXADECIMAL=`echo -n "$AUDIO_FILE" | md5sum | cut -d " " -f 1`
+VERSION_CURRENT="0.6"
+PORT="9999"
+IP_SERVER="localhost"
 
-clear 
+clear
 
-echo "cliente de rectp $VERSION_CURRENT"
 
-echo "1.0.SEND envia la cabecera al servidor"
-IP_LOCAL=`ip -4 addr | grep "scope global" | awk "{print $2}`
+echo "Cliente del protocolo RECTP v$VERSION_CURRENT"
+
+echo "1. SEND. Enviamos la cabecera al servidor"
+
+IP_LOCAL=`ip -4 addr | grep "scope global" | awk '{print $2}' | cut -d "/" -f 1`
+
 sleep 1
 echo "RECTP $VERSION_CURRENT $IP_LOCAL" | nc $IP_SERVER -q 0 $PORT
 
-echo "2.0 Listen:"
+echo "2. LISTEN"
 RESPONSE=`nc -l -p $PORT`
 
+echo "5. TEST. Header Response"
 
-echo "5.0 Test Header Response"
 if [ "$RESPONSE" != "HEADER_OK" ]
 then
-echo "ERROR 1: Cabecera mal formada"
+	echo "Error 1: Cabeceras mal formadas"
 
-exit 1
+	exit 1
 fi
-echo "6.SEND Nombre de archivo"
-sleep 1
-echo "FILE_NAME $AUDIO_FILE" | nc $IP_SERVER -q 0 $PORT
 
-echo "7.LISTEN FILE_NAME_OK"
+echo "6. SEND. Nombre de archivo y hash"
+
+sleep 1
+echo "FILE_NAME $AUDIO_FILE" "$HASH_MD5_HEXADECIMAL"| nc $IP_SERVER -q 0 $PORT
+
+
+
+
+echo "7.0 LISTEN. FILE_NAME_OK"
 
 RESPONSE=`nc -l -p $PORT`
 
-
-echo "10.TEST FILE_NAME_OK"
+echo "10.0 TEST. FILE_NAME_OK"
 
 if [ "$RESPONSE" != "FILE_NAME_OK" ]
 then
-echo "Error 2: Nombre de archivo incorrecto o mal formado"
-exit 2
+	echo "Error 2: Nombre de archivo incorrecto o mal formado"
+	exit 2
 fi
 
-echo "11.SEND FILE DATA"
+
+echo "11 LISTEN. HASH_OK"
+RESPONSE=`nc -l -p $PORT`
+
+echo "12 TEST HASH_OK"
+
+if [ "$RESPONSE" != "HASH_OK" ]
+then
+	echo "Error 3: Hash del archivo erroneo"
+	exit 3
+fi
+
+
+echo "15. SEND. FILE DATA"
+
 sleep 1
 cat audio.wav | nc $IP_SERVER -q 0 $PORT
 
-echo "12.LISTEN"
+echo "16. LISTEN"
 
 RESPONSE=`nc -l -p $PORT`
 
-echo "15.0 TEST AND END"
-
-
+echo "17. TEST AND END"
 
 if [ "$RESPONSE" != "FILE_DATA_OK" ]
 then
-echo "ERROR 3: Datos de archivo corruptos"
+	echo "ERROR 4: Datos del archivo corruptos"
 
-exit 3
+	exit 4
 fi
 
-echo "Fin de comunicación"
+echo "Fin de comuniación"
+
 exit 0
--- rectp/servidor.sh
+++ rectp/servidor.sh
@@ -1,83 +1,133 @@
-#!/bin/bash
-PORT="9999"
+#/bin/bash
+
 VERSION_CURRENT="0.6"
+PORT="9999"
 IP_CLIENT="localhost"
 SERVER_DIR="server"
+
 
 clear
 
 mkdir -p $SERVER_DIR
-echo "servidor de rectp v$VERSION_CURRENT"
-echo "0. Listen"
-DATA=`nc -l -p $PORT`
-HEADER=`echo $DATA | cut  -d " " -f 1`
 
-echo "3.0 Test"
-if [ $HEADER != "RECTP" ]
+echo "Servidor de RECTP v$VERSION_CURRENT"
+
+IP_LOCAL=`ip -4 addr | grep "scope global" | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1`
+
+echo "IP Local: $IP_LOCAL"
+
+echo "0. LISTEN. HEADER"
+
+DATA=`nc -l -p $PORT`
+
+echo "3.1. TEST. Datos"
+
+HEADER=`echo $DATA | cut -d " " -f 1`
+
+if [ "$HEADER" != "RECTP" ]
 then
-  echo "ERROR 1: Cabecera erronea"
-sleep 1
-echo "HEADER_KO" | nc $IP_CLIENT -q 0 $PORT
-  exit 1
-else  
-  echo "Cabecera correcta"
+	echo "ERROR 1: Cabecera errónea"
+
+	sleep 1
+	echo "HEADER_KO" | nc $IP_CLIENT -q 0 $PORT
+
+	exit 1
 fi
 
 VERSION=`echo $DATA | cut -d " " -f 2`
-if [ $VERSION != "$VERSION_CURRENT" ]
-then	
 
-echo "ERROR 2: Version erronea"
-sleep 1
-echo "HEADER_KO" | nc $IP_CLIENT -q 0 $PORT
-exit 2
+if [ "$VERSION" != "$VERSION_CURRENT" ]
+then
+	echo "ERROR 2: Versión errónea"
+
+	sleep 1
+	echo "HEADER_KO" | nc $IP_CLIENT -q 0 $PORT
+
+	exit 2
 fi
+
+IP_CLIENT=`echo $DATA | cut -d " " -f 3`
+
+if [ "$IP_CLIENT" == "" ]
+then
+	echo "Error 3: IP de cliente mal formada ($IP_CLIENT)"
+
+	exit 3
+fi
+
+
 
 echo "3.2. RESPONSE. Enviando HEADER_OK"
 
+sleep 1
 echo "HEADER_OK" | nc $IP_CLIENT -q 0 $PORT
 
-echo "4.0 Listen header"
+echo "4. LISTEN. Nombre de archivo"
+
 DATA=`nc -l -p $PORT`
 
-echo "8.0. FILE NAME"
+echo "8. FILE NAME and Hash"
+
 echo "8.1 TEST"
+
+FILE_NAME=`echo $DATA | cut -d " " -f 2`
+HASH_FILE_RECIEVED=`echo -n "$DATA" | cut -d " " -f 3`
+HASH_FILE=`echo -n "$FILE_NAME" | md5sum | cut -d " " -f 1`
 FILE_NAME_PREFIX=`echo $DATA | cut -d " " -f 1`
 
 
-if [ "$FILE_NAME_PREFIX" != "$FILE_NAME" ]
+
+if [ "$FILE_NAME_PREFIX" != "FILE_NAME" ]
 then
+	echo "Error 4: Prefijo FILE_NAME incorrecto ($FILE_NAME_PREFIX)"
 
-echo "ERROR 3: Prefijo FILE_NAME incorrecto ($FILE_NAME_PREFIX)"
+	sleep 1
+	echo "FILE_NAME_KO" | nc $IP_CLIENT -q 0 $PORT
 
-sleep 1
-echo "FILE_NAME_KO" | nc $IP_CLIENT -q 0 $PORT
-exit 3
-
-FILE_NAME=`echo $DATA | cut -d " " -f 2`
-echo "File name: $FILE_NAME"
-
+	exit 4
 fi
 
 
+echo "File Name: $FILE_NAME"
 
-echo "8.2.RESPONSE FILE_NAME_OK"
+echo "8.2 RESPONSE FILE_NAME_OK"
+
+sleep 1
 echo "FILE_NAME_OK" | nc $IP_CLIENT -q 0 $PORT
 
-echo "9.LISTEN FILE DATA"
 
+if [ "$HASH_FILE_RECIEVED" != "$HASH_FILE" ]
+then
+	echo "Error 5: Hash erroneo"
+
+	sleep 1
+
+	echo "HASH_KO" | nc $IP_CLIENT -q 0 $PORT
+
+	exit 5
+fi
+
+echo "Hash: $HASH_FILE"
+
+
+echo "8.3 RESPONSE HASH_OK"
+sleep 1
+echo "HASH_OK" | nc $IP_CLIENT -q 0 $PORT
+
+
+
+echo "9. LISTEN FILE DATA"
 echo "13. STORE FILE DATA"
-
-
 
 nc -l -p $PORT > $SERVER_DIR/$FILE_NAME
 
-echo "14.SEND FILE_DATA_OK"
-sleep 1
+echo "14. SEND. FILE_DATA_OK"
 
+sleep 1
 echo "FILE_DATA_OK" | nc $IP_CLIENT -q 0 $PORT
-aplay $SERVER_DIR/$FILE_NAME
+
 echo "Fin de comunicación"
 
+aplay $SERVER_DIR/$FILE_NAME
 
 exit 0
